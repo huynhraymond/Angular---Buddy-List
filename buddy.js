89e147077bae0a8601f62e9209a61f16155994ec
stevenchu_sj <huynhraymond@yahoo.com>
2015-06-22 11:25:54 -0700

    Buddy List 1v

---
+++ buddy.js
@@ -0,0 +1,6 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('button.addNewBuddy').addEventListener('click', function() {
+        document.querySelector('div.add-buddy-form').style.display = 'block';
+    });
+});
